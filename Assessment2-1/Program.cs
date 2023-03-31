3e80bb13bc993d8e22e9f297018f9765c8373acc
Allen Lewis <allenlewis32@gmail.com>
2023-03-31 15:30:22 +0530

    Add project files.

---
+++ Assessment2-1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace Assessment2_1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the existing plate: ");
+            string plate = Console.ReadLine();
+            Console.Write("Enter the grouping number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Program.LicensePlate(plate, num));
+        }
+        public static string LicensePlate(string old, int n)
+        {
+            old = old.Replace("-", "").ToUpper();
+            StringBuilder sb = new StringBuilder();
+            int l = old.Length;
+            int remaining = l % n;
+            sb.Append(old.Substring(0, remaining));
+            for (int i = remaining; i < l; i += n)
+            {
+                if (i != 0) { sb.Append('-'); }
+                sb.Append(old.Substring(i, n));
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ Assessment2-1Tests/ProgramTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assessment2_1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment2_1.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void LicensePlateTest_A()
+        {
+            Assert.AreEqual(Program.LicensePlate("5F3Z-2e-9-w", 4), "5F3Z-2E9W");
+        }
+        [TestMethod()]
+        public void LicensePlateTest_B()
+        {
+            Assert.AreEqual(Program.LicensePlate("2-5g-3-J", 2), "2-5G-3J");
+        }
+        [TestMethod()]
+        public void LicensePlateTest_C()
+        {
+            Assert.AreEqual(Program.LicensePlate("2-4A0r7-4k", 3), "24-A0R-74K");
+        }
+        [TestMethod()]
+        public void LicensePlateTest_D()
+        {
+            Assert.AreEqual(Program.LicensePlate("nlj-206-fv", 3), "NL-J20-6FV");
+        }
+    }
+}
